47f7b7f6aec2675e2ddb4bfcbd3ad3d80b58d33f
Hanna Szafranska <109690308+hs763@users.noreply.github.com>
2025-03-22 09:21:38 +0000

    Update spipe_P1_300.sh

--- spipe_P1_300.sh
+++ spipe_P1_300.sh
@@ -1,3 +1,20 @@
+#instaling spipe on the cluster https://support.parsebiosciences.com/hc/en-us/articles/23060102930580-Pipeline-Installation-Current-Version
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh 
+bash Miniconda3-latest-Linux-x86_64.sh
+
+#location: /lmb/home/hannas/miniconda3
+export PATH="/lmb/home/hannas/miniconda3/bin:$PATH"
+
+conda create -n spipe conda-forge::python==3.10
+conda activate spipe
+
+unzip ParseBiosciences-Pipeline.1.3.1.zip 
+cd ParseBiosciences-Pipeline.1.3.1
+bash ./install_dependencies_conda.sh -i -y
+pip install --no-cache-dir ./
+
+split-pipe
+
 
 #!/bin/bash
 
