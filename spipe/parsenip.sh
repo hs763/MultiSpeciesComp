e0b23957f6125e0d3a1ec242b11fcb96f17a42a1
Hanna Szafranska <109690308+hs763@users.noreply.github.com>
2025-04-18 15:55:16 +0100

    Update parsenip.sh

--- spipe/parsenip.sh
+++ spipe/parsenip.sh
@@ -17,3 +17,13 @@ nohup split-pipe \
     --fq2 /mnt/SLX-25498.ParseWT21.22K75GLT4.r_2.fq.gz \
     --output_dir output \
     --samp_list /mnt/SampleLoadingTable_MultiSpieciesComp.txt > log_parse.out &
+
+
+#to adress the multimapping question I will rerun the pipeline with jsut the human reference genome
+cd /cephfs2/hannas/MultiSpeciesComp/final/expdata/parsnip_only_human_ref
+
+nextflow run -config /public/singularity/containers/nextflow/lmb-nextflow/genomes.config,/public/singularity/containers/nextflow/ParseNIP/nextflow.config \
+-profile lmb_cluster /public/singularity/containers/nextflow/ParseNIP/main.nf --genome homo_sapiens.GRCh38.release_102 \
+--genome_name homo_sapiens.GRCh38.release_102 \
+--samp_list /cephfs2/hannas/MultiSpeciesComp/final/expdata/parsnip_only_human_ref/SampleLoadingTable_MultiSpieciesComp.txt \
+--fastq /cephfs2/hannas/MultiSpeciesComp/final/expdata/FASTQ --chemistry v1 -bg
